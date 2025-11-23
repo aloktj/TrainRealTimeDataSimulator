5ba0d5da0b7077bba89ea7723fcac51f320b721f
aloktj <aloktj97@gmail.com>
2025-11-23 12:13:56 +0530

    Guard tinyxml2 dependency and expose realtime route

--- include/trdp_sim/realtime_stream.hpp
+++ include/trdp_sim/realtime_stream.hpp
@@ -8,6 +8,8 @@
 class RealtimeStream : public drogon::WebSocketController<RealtimeStream>
 {
   public:
+    RealtimeStream() = default;
+
     static void bootstrap(auth::AuthManager* auth, realtime::RealtimeHub* hub);
 
     void handleNewConnection(const drogon::HttpRequestPtr& req, const drogon::WebSocketConnectionPtr& conn) override;
@@ -21,13 +23,13 @@ class RealtimeStream : public drogon::WebSocketController<RealtimeStream>
     void handleMessage(const drogon::WebSocketConnectionPtr& conn, const std::string& message,
                       const drogon::WebSocketMessageType& type);
 
-  private:
-    void handleMessageImpl(const drogon::WebSocketConnectionPtr& conn, const std::string& message,
-                           const drogon::WebSocketMessageType& type);
-
     WS_PATH_LIST_BEGIN
     WS_PATH_ADD("/api/ws/realtime", drogon::Get, drogon::Options);
     WS_PATH_LIST_END
+
+  private:
+    void handleMessageImpl(const drogon::WebSocketConnectionPtr& conn, const std::string& message,
+                           const drogon::WebSocketMessageType& type);
 
     static auth::AuthManager*     s_auth;
     static realtime::RealtimeHub* s_hub;
